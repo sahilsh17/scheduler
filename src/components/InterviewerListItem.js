12ecb7dd20dd1ca194c2a32ebf90982913bc1ad7
Sahil Sharma <sahil.sharma237333@gmail.com>
2020-12-08 15:16:58 -0800

    refactored code by sending interviewer.id in function through setInterviewer in line 13

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -3,20 +3,20 @@ import "components/InterviewerListItem.scss";
 import classNames from "classnames";
 
 export default function InterviewerListItem(props) {
-  const interviewersListItemClass = classNames("interviwers__item", {
-    "interviewers__item-image": props.image,
+  const interviewersListItemClass = classNames("interviewers__item", {
+   
     "interviewers__item--selected": props.selected
   })
 
   return (
-    <li className={interviewersListItemClass} onClick={()=>props.setInterviewer(props.name)}>
+    <li className={interviewersListItemClass} onClick={props.setInterviewer}>
 
   <img
-    className={interviewersListItemClass}
+     className="interviewers__item-image"
     src={props.avatar}
     alt="Sylvia Palmer"
   />
-  {props.name}
+  {props.selected && props.name}
 </li>
   );
 }
