d0882d50f0d573904cc7ca4a785b26f98c0d1619
Sahil Sharma <sahil.sharma237333@gmail.com>
2020-12-07 16:17:43 -0800

    formatSpots function defined to display spots in new way

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -6,11 +6,19 @@ export default function DayListItem(props) {
    'day-list__item--selected': props.selected,
    'day-list__item--full': props.spots === 0
   });
-  
+  const formatSpots = function(spots) {
+    if (spots === 0) {
+      return 'no spots remaining'
+    } else if (spots === 1) {
+      return '1 spot remaining'
+    } else {
+      return `${spots} spots remaining`;
+    }
+  }
   return (
     <li className={DayListItemClass} onClick={() => props.setDay(props.name)}>
       <h2 >{props.name}</h2>
-      <h3 >{props.spots}</h3>
+      <h3 >{formatSpots(props.spots)}</h3>
     </li>
   );
 }
