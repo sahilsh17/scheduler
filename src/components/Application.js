972d4c12d7d6d77df8b2ba23385db4d3ba56cfbf
Sahil Sharma <sahil.sharma237333@gmail.com>
2020-12-09 15:39:17 -0800

    imported axios,useEffect and fetched days array data from api server, deleted mock days array

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,24 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+import axios from "axios";
+
+
 const appointments = [
   {
     id: 1,
@@ -63,7 +49,14 @@ const appointments = [
   }
 ];
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+  const [day, setDay] = useState('Monday');
+  useEffect(()=>{
+    axios.get('http://localhost:8001/api/days')
+    .then((response) => {
+      setDays(response.data);
+    })
+  },[]);
   return (
     <main className="layout">
       <section className="sidebar">
