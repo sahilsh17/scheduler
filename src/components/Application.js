eab36f65eb87bc41780c57ceb9a6b4e84d604b51
Sahil Sharma <sahil.sharma237333@gmail.com>
2020-12-09 17:37:03 -0800

    state is declared as object now

--- src/components/Application.js
+++ src/components/Application.js
@@ -49,14 +49,21 @@ const appointments = [
   }
 ];
 export default function Application(props) {
-  const [days, setDays] = useState([]);
-  const [day, setDay] = useState('Monday');
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    // you may put the line below, but will have to remove/comment hardcoded appointments variable
+    appointments: {}
+  });
+  const setDay = day => setState({ ...state, day });
+  const setDays = days =>setState(prev => ({ ...prev, days }));
   useEffect(()=>{
     axios.get('http://localhost:8001/api/days')
     .then((response) => {
       setDays(response.data);
     })
   },[]);
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -67,8 +74,8 @@ export default function Application(props) {
 />
 <hr className="sidebar__separator sidebar--centered" />
 <nav className="sidebar__menu"><DayList
-  days={days}
-  day={day}
+  days={state.days}
+  day={state.day}
   setDay={setDay}
 /></nav>
 <img
