61d8bf5fa9cc5bd2b87544bc09fee88a2d0a663d
Sahil Sharma <sahil.sharma237333@gmail.com>
2020-12-09 17:35:35 -0800

    added selector function and test file

---
+++ src/helpers/selectors.js
@@ -0,0 +1,16 @@
+export function getAppointmentsForDay(state, day) {
+  //... returns an array of appointments for that day
+  const filterDay = state.days.filter(dayItem => dayItem.name === day);
+  if(filterDay.length === 0) {
+    return [];
+  }
+  let appoinmentForDay = [];
+    filterDay[0].appointments.forEach(AppoinmentID => {
+    for(let appoinment in state.appointments) {
+      if (AppoinmentID == appoinment) {
+        appoinmentForDay.push(state.appointments[appoinment]);
+      }
+    }
+  }) 
+  return appoinmentForDay;
+} 
