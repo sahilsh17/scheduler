c8be57459836f5a941867c3bef634d41a89dcc34
Sahil Sharma <sahil.sharma237333@gmail.com>
2020-12-10 16:12:16 -0800

    useVisualMode to set different modes and rendered components based on modes conditions

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,18 +3,40 @@ import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import useVisualMode from "hooks/useVisualMode";
+import Form from "components/Appointment/Form";
 
 export default function Appointment(props) {
-  
- if(props.interview) {
-   return (<Fragment><Header time={props.time}/><Show student={props.interview.student} interviewer={props.interview.interviewer}/></Fragment>);
- }
- 
- return (<Fragment><Header time={props.time} /><Empty/></Fragment>)
-   
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+  return (
+    <Fragment>
+      {mode === EMPTY && <Fragment><Header time={props.time} /><Empty onAdd={() => transition('CREATE')} /></Fragment>}
+      {mode === SHOW && (
+        <Fragment><Header time={props.time} /><Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        /> </Fragment>
+      )}
+      {mode === CREATE && (
+        <Fragment><Header time={props.time} /><Form onCancel={()=> back()}  interviewers ={props.interviewers}/> </Fragment>
+      )}
+    </Fragment>
 
- 
-  
-  
-    
+  )
 }
+//  if(props.interview) {
+//    return (<Fragment><Header time={props.time}/><Show student={props.interview.student} interviewer={props.interview.interviewer}/></Fragment>);
+//  }
+
+//  return (<Fragment><Header time={props.time} /><Empty/></Fragment>)
+
+
+
+
+
+
