096a67091759d89b0633d0fc9eab2ce6b8471235
Sahil Sharma <sahil.sharma237333@gmail.com>
2020-12-12 01:02:37 -0800

    new function save added to create an interview object and passed to bookInterview to book interview

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,25 +5,41 @@ import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
 import useVisualMode from "hooks/useVisualMode";
 import Form from "components/Appointment/Form";
+import Status from "components/Appointment/Status";
 
 export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
+  const SAVING = 'SAVING';
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
+ 
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+    transition('SAVING');
+    props.bookInterview(props.id,interview)
+    .then(()=>{transition("SHOW");})
+    
+  }
   return (
+    
     <Fragment>
+      {mode === SAVING && <Fragment><Header time={props.time} /><Status message='SAVING' /></Fragment>}
       {mode === EMPTY && <Fragment><Header time={props.time} /><Empty onAdd={() => transition('CREATE')} /></Fragment>}
       {mode === SHOW && (
         <Fragment><Header time={props.time} /><Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
+         
         /> </Fragment>
       )}
       {mode === CREATE && (
-        <Fragment><Header time={props.time} /><Form onCancel={()=> back()}  interviewers ={props.interviewers}/> </Fragment>
+        <Fragment><Header time={props.time} /><Form onSave={save}  onCancel={()=> back()}  interviewers ={props.interviewers} /> </Fragment>
       )}
     </Fragment>
 
