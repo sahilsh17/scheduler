9fb5be916f58c71912cbcb0a7eb4353d7a4adcf6
Sahil Sharma <sahil.sharma237333@gmail.com>
2020-12-12 22:14:55 -0800

    Error mode is added by using .catch in promise

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,6 +7,7 @@ import useVisualMode from "hooks/useVisualMode";
 import Form from "components/Appointment/Form";
 import Status from "components/Appointment/Status";
 import Confirm from "components/Appointment/Confirm";
+import Error from "components/Appointment/Error";
 
 export default function Appointment(props) {
   const EMPTY = "EMPTY";
@@ -16,49 +17,53 @@ export default function Appointment(props) {
   const DELETING = 'DELETING';
   const CONFIRM = 'CONFIRM';
   const EDIT = "EDIT";
+  const ERROR_SAVING = "ERROR_SAVING";
+  const ERROR_DELETING = "ERROR_DELETING";
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
- 
+
   function save(name, interviewer) {
     const interview = {
       student: name,
       interviewer
     };
     transition('SAVING');
-    props.bookInterview(props.id,interview)
-    .then(()=>{transition("SHOW");});
-    
+    props.bookInterview(props.id, interview)
+      .then(() => transition("SHOW"))
+      .catch(() => transition("ERROR_SAVING"));
   }
 
-  function Delete(){
+  function Delete() {
     const interview = null;
-    transition('DELETING');
+    transition('DELETING',true);
     console.log(props.id);
     props.cancelInterview(props.id, interview)
-    .then(()=>{transition("EMPTY");});
+      .then(() => { transition("EMPTY"); })
+      .catch(() => transition('ERROR_DELETING',true));
   }
   return (
-    
+
     <Fragment>
       {mode === SAVING && <Fragment><Header time={props.time} /><Status message='SAVING' /></Fragment>}
       {mode === DELETING && <Fragment><Header time={props.time} /><Status message='Deleting' /></Fragment>}
-      {mode === CONFIRM && <Fragment><Header time={props.time} /><Confirm message="Are you sure you want to delete" onConfirm = {Delete}          onCancel={()=>transition('SHOW')}  /></Fragment>}
-
+      {mode === CONFIRM && <Fragment><Header time={props.time} /><Confirm message="Are you sure you want to delete" onConfirm={Delete} onCancel={() => transition('SHOW')} /></Fragment>}
+      {mode === ERROR_SAVING && <Fragment><Header time={props.time} /><Error onClose={() => back()}message='ERROR SAVING' /></Fragment>}
+      {mode === ERROR_DELETING && <Fragment><Header time={props.time} /><Error onClose={() => back()} message='ERROR DELETING' /></Fragment>}
       {mode === EMPTY && <Fragment><Header time={props.time} /><Empty onAdd={() => transition('CREATE')} /></Fragment>}
       {mode === SHOW && (
         <Fragment><Header time={props.time} /><Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
-          onDelete = {()=> {transition('CONFIRM')}}
-          onEdit = {()=>transition('EDIT')}
+          onDelete={() => { transition('CONFIRM') }}
+          onEdit={() => transition('EDIT')}
         /> </Fragment>
       )}
       {mode === CREATE && (
-        <Fragment><Header time={props.time} /><Form  onSave={save}  onCancel={()=> back()}  interviewers ={props.interviewers} /> </Fragment>
+        <Fragment><Header time={props.time} /><Form onSave={save} onCancel={() => back()} interviewers={props.interviewers} /> </Fragment>
       )}
-      {mode === EDIT && <Fragment><Header time={props.time} /><Form onSave={save} onCancel={()=> back()} name={props.interview.student} interviewer={props.interview.interviewer.id} interviewers ={props.interviewers}/></Fragment>}
+      {mode === EDIT && <Fragment><Header time={props.time} /><Form onSave={save} onCancel={() => back()} name={props.interview.student} interviewer={props.interview.interviewer.id} interviewers={props.interviewers} /></Fragment>}
 
     </Fragment>
 
