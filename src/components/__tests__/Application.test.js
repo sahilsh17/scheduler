bd4a3155dbe001211b575a3a45918fdb0792f43f
Sahil Sharma <sahil.sharma237333@gmail.com>
2020-12-15 16:58:53 -0800

    fixed bug- alt field assigned to props.name

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId } from "@testing-library/react";
 
 import Application from "components/Application";
 
 
 
 afterEach(cleanup);
-
-it("changes the schedule when a new day is selected", async () => {
+describe("Application", () => {
+  it("changes the schedule when a new day is selected", async () => {
   const { getByText } = render(<Application />);
 
   await waitForElement(() => getByText("Monday"));
@@ -16,4 +16,12 @@ it("changes the schedule when a new day is selected", async () => {
   fireEvent.click(getByText("Tuesday"));
 
   expect(getByText("Leopold Silvers")).toBeInTheDocument();
+
+  });
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1",async ()=>{
+    const { container } = render (<Application/>);
+    await waitForElement(() => getByText(container,"Archie Cohen"));
+    const appointments = getAllByTestId(container, "appointment");
+    console.log(prettyDOM(appointments));
+  })
 });
