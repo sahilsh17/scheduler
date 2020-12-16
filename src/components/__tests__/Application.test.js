845945b3ad73a41760625de68b82dbc1a4c7e708
Sahil Sharma <sahil.sharma237333@gmail.com>
2020-12-15 17:00:46 -0800

    created a test to check that user can see the all appointments and can book interview on an empty slot

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId , getByAltText, getByPlaceholderText } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -19,9 +19,20 @@ describe("Application", () => {
 
   });
   it("loads data, books an interview and reduces the spots remaining for the first day by 1",async ()=>{
-    const { container } = render (<Application/>);
+    const { container} = render (<Application/>);
     await waitForElement(() => getByText(container,"Archie Cohen"));
     const appointments = getAllByTestId(container, "appointment");
-    console.log(prettyDOM(appointments));
+    const appointment = appointments[0];
+    
+    fireEvent.click(getByAltText(appointment, "Add"));
+
+fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+  target: { value: "Lydia Miller-Jones" }
+});
+ fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+ console.log(prettyDOM(appointment));
+
+fireEvent.click(getByText(appointment, "Save"));
+
   })
 });
