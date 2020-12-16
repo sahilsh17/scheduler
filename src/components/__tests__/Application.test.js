e34ebb7be51f16bad9f5151f414f7b0a3956db35
Sahil Sharma <sahil.sharma237333@gmail.com>
2020-12-15 21:10:23 -0800

    added new test to check if the error message is displayed when put request does not go through while saving new interview

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -4,7 +4,7 @@ import { render, cleanup, waitForElement, fireEvent, getByText, queryByText, pre
 
 import Application from "components/Application";
 
-
+import axios from "axios";
 
 afterEach(cleanup);
 describe("Application", () => {
@@ -56,7 +56,7 @@ describe("Application", () => {
 
   });
 
-  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+  xit("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
 
     //1. Render the Application container
     const { container, debug } = render(<Application />);
@@ -89,5 +89,21 @@ describe("Application", () => {
     
     expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
 
+  });
+
+  it("shows the save error when failing to save an appointment", async() => {
+    axios.put.mockRejectedValueOnce();
+    const { container,debug } = render(<Application />);
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[0];
+
+    fireEvent.click(getByAltText(appointment, "Add"));
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+    fireEvent.click(getByText(appointment, "Save"));
+    expect(getByText(appointment, "SAVING")).toBeInTheDocument();
+    await waitForElement(() => getByText(appointment,"ERROR SAVING"));
   });
 });
