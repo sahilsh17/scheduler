7563aa7754d07cbe80df7a31a6eda0ec5059d3ff
Sahil Sharma <sahil.sharma237333@gmail.com>
2020-12-16 14:31:41 -0800

    modified test should navigate to Tuesday using a data-testid of the day element

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -4,7 +4,8 @@ describe("Navigation", () => {
   });
   it("should navigate to Tuesday", () => {
     cy.visit("/");
-    cy.contains("Tuesday")
-    .click();
-  })
+    
+    cy.contains("[data-testid=day]","Tuesday").click()
+  .should("have.class", "day-list__item--selected");
+  });
 });
